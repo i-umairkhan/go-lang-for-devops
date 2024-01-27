a58301846c4c8d6352a883fee9fac058a2b478ef
Umair Khan <m.umairkhan.co@proton.me>
2024-01-27 22:32:16 +0500

    ADDED contol strucutres

---
+++ day-2/control-structure.go
@@ -0,0 +1,23 @@
+package main 
+
+import "fmt"
+
+func main(){
+	// If else control structures
+	x := 10
+	y := 20
+
+	if x > 5 {
+		fmt.Println("x is grater than 5")
+	} else if x < 5 {
+		fmt.Println("x is less than 5")
+	} else {
+		fmt.Println("x is equal to 10") 
+	}
+
+	// Control structures with initialization
+
+	if y = 10; x == 10 || y == 20 {
+		fmt.Println("x is 10 and y is 20")
+	}
+}
